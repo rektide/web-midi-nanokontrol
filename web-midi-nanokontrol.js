3d152667df38ed52d8939422ebaed4ff0fcb4387
rektide de la faye <rektide@voodoowarez.com>
2018-01-18 21:34:07 -0500

    Convert the 1x high bit block + 7x 7 bit messages format into 8 bits.
    Korg data dump packs 8 bit data values into a stream of 7 bytes. This
    work decodes that data stream.
    
    In the format, a lead block holds the 8th bit of the next 7 bytes. I
    call this the "high" byte. To convert into an array of normal bytes,
    read all non-high bytes, and lookup & read in the highest bit from
    the preceeding high byte.

--- web-midi-nanokontrol.js
+++ web-midi-nanokontrol.js
@@ -75,7 +75,35 @@ class NanoKontrol2 extends EventEmitter{
 		var output= await this.consumeOutput( optionalOutputName)
 		output.send( new messages.currentSceneDataDumpRequest().toBytes())
 
-		return await readResponse
+		var scene= await readResponse
+		scene.data= NanoKontrol2.bitMash( scene.data)
+		return scene
+	}
+	static bitMash( bytes){
+		var
+		  n= Math.floor( bytes.length* 7/ 8),
+		  output= new Uint8Array( n)
+		var
+		  highPos= 0, // which byte is the current high byte? data starts with a high byte: 0.
+		  highCount= 1, // how many high bytes have we seen? we start by having seen the first one: 1.
+		  highBitShifts= 7 // how many left barrel shifts to make the current high bit the 8th bit? 0b1 needs to become 0b1000000: 7
+		for( var i= 0; i< n; ++i){
+			var
+			  val= bytes[ i+ highCount], // base value
+			  highBit= (bytes[ highPos]<< highBitShifts)& 0x80 // get the high byte, shift our bit into position, and mask it
+			// take the base value and graft on the high bit
+			output[ i]= val+ highBit
+			// move on to the next bit in the high byte, which will require one less shift
+			--highBitShifts
+			// see if there are remaining bits
+			if(highBitShifts< 1){ // the last bit is 0, unused
+				// we've used all 7 bits. the next byte will be a new high byte. advance through it.
+				++highCount // there is a new high byte
+				highPos+= 8 // we've just read a high byte and seven bytes, so we are +8
+				highBitShifts= 7 // high byte is new, has 7 fresh bits. start with lowest bit.
+			}
+		}
+		return output
 	}
 	async readOne( messageKlass){
 		var
