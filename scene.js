c1ce4aae720ece39a53b4e2423d0514a11d1379e
rektide de la faye <rektide@voodoowarez.com>
2018-01-25 19:00:21 -0500

    Wrap up scene transports, & daw.

--- scene.js
+++ scene.js
@@ -3,7 +3,8 @@
 var
   _assign= ["none", "cc", "note"],
   _behavior= ["momentary", "toggle"],
-  _bool= ["disable", "enable"];
+  _bool= ["disable", "enable"],
+  _channel= [,,,,,,,,,,,,,,,"global"]
 
 function bool(){
 	return {
@@ -15,15 +16,15 @@ function bool(){
 //// DATA DEFINITIONS ////
 
 // common parameters
-var common= {
+export let common= {
 	globalMidiChannel: {max: 15},
 	contorlMode: {values: ["cc", "cubase", "dp", "live", "protools", "sonar"]},
 	ledMode: {values: ["internal", "external"]}
 }
 
 // control group params
-var controlGroup= {
-	groupMidiChannel: {values: [,,,,,,,,,,,,,,,"global"]},
+export let controlGroup= {
+	groupMidiChannel: {values: _channel},
 	sliderAssign: bool(),
 	reserved: {max: 0},
 	sliderCcNote: {},
@@ -57,23 +58,62 @@ var controlGroup= {
 }
 
 // index where each control group begins
-var controlGroupOffsets= [3, 34, 65, 96, 127, 158, 189, 220]
+export let controlGroupOffsets=[ 3, 34, 65, 96, 127, 158, 189, 220]
 
-//// BUILD EXPORTS ////
+export let controlGroups= [ 1, 2, 3, 4, 5, 6, 7, 8]
 
-export let scene= new Array(338)
+export let transportChannel= normalize({ index: 251, values: _channel}, "transportChannl")
+
+export let transport= {
+	assign: {values: _assign},
+	behavior: {values: _behavior},
+	ccNote: {},
+	off: {},
+	on: {},
+	reserved: {max: 0}
+}
+
+
+export let transportOffsets=[ 252, 258, 264, 270, 276, 282, 288, 294, 300, 306, 312]
+
+export let transports={
+	"prev": {},
+	"next": {},
+	"cycle": {},
+	"marker": {},
+	"prevMarker": {},
+	"nextMarker": {},
+	"rew": {},
+	"ff": {},
+	"stop": {},
+	"play": {},
+	"rec": {}
+}
+
+export let customDAW={ max: 255, valid: [ 0, 42, 42, 46, 47, 48, 49, 50, 127, 255]}
+
+// an array with the definition of each byte
+// the above data is used to fill out this array
+export let scene= new Array( 338)
 export default scene
+
+//// BUILD SCENE ////
 
 function normalize( o, name){
 	o.name= name
 	if( o.values&& !o.max){
-		o.max= o.values.length- 1
+		o.max= o.values.length- 1 // calculate max
 	}
 	if( o.max=== undefined){
-		o.max= 127
+		o.max= 127 // default max
 	}
 	if( o.min=== undefined){
-		o.min= 0
+		o.min= 0 // default min
+	}
+	for( var i in o.values){
+		if( o.values[ i]=== undefined){
+			o.values[ i]= i // values are identity if not defined
+		}
 	}
 	return o
 }
@@ -87,10 +127,10 @@ for( var index= 0; index< commonKeys.length; ++index){
 	o.index= index
 	scene[ index]= o
 }
+
 // control groups
 var controlGroupKeys= Object.keys( controlGroup)
 for( var keyNum= 0; keyNum< controlGroupKeys.length; ++keyNum){
-//for( var keyNum in controlGroupKeys){
 	var
 	  name= controlGroupKeys[ keyNum],
 	  o= normalize( controlGroup[ name], name)
@@ -102,4 +142,32 @@ for( var keyNum= 0; keyNum< controlGroupKeys.length; ++keyNum){
 		  index= keyNum+ offset
 		scene[ index]= Object.assign({ channel: ++channel, index}, o)
 	}
+}
+
+// transport
+scene[ 251]= transportChannel
+// transport groups
+var
+  transportKeys= Object.keys( transport), // fields in a transport
+  transportNames= Object.keys( transports) // names of all transports
+for( var keyNum= 0; keyNum< transportKeys.length; ++keyNum){
+	var
+	  name= transportKeys[ keyNum],
+	  o= normalize( transport[ name], name)
+	// each transport group is repeated across all 8 channels,
+	// with each channel starting at it's own offset
+	for( var transportIndex= 0; transportIndex< transportOffsets.length;){ // we increment transportIndex in the final Object.assign
+		var
+		  offset= transportOffsets[ transportIndex],
+		  index= keyNum+ offset,
+		  transportName= transportNames[ transportIndex],
+		  val= Object.assign({ index: ++transportIndex, index}, o)
+		scene[ index]= val
+		transports[ transportName][ name]= val
+	}
+}
+
+// custom DAW
+for( var i= 318; i<= 322; ++i){
+	scene[ i]= normalize( customDAW, "customDAW")
 }
